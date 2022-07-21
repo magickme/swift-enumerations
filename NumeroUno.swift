27da55b57f410fea965af1108493951e7787e436
Jason Louv <jason@ultraculture.org>
2022-07-20 23:21:46 -0500

    Logic complete

--- NumeroUno.swift
+++ NumeroUno.swift
@@ -12,3 +12,48 @@ enum ProductType: String, CaseIterable {
     }
 }
 
+enum Edition: String {
+    case basic
+    case premium
+    case ultimate
+
+    mutating func upgrade() {
+        switch self {
+            case .basic:
+                self = .premium
+            case .premium:
+                self = .ultimate
+            case .ultimate:
+                print("Can't upgrade further")
+        }
+    }
+}
+
+enum DeliveryMethod {
+    case cloudDigital(isLifetime: Bool)
+    case shipping(weight: Int)
+    
+    var shippingCost: Int {
+        switch self {
+            case .shipping(let weight):
+                return weight * 2
+            case .cloudDigital:
+                return 0
+        }
+    }
+}
+
+func sendOrderConfirmation(
+    of productType: ProductType, in edition: Edition, by deliveryMethod: DeliveryMethod) {
+        print("Thank you for purchasing \(edition.rawValue) edition of \(productType.rawValue)")
+        switch deliveryMethod {
+            case .shipping:
+                print("Your order will be shipped to you at a cost of $\(deliveryMethod.shippingCost)")
+            case .cloudDigital(let isLifetime):
+                if isLifetime {
+                    print("You have lifetime cloud access.")
+                } else {
+                    print("You can access your subscription information in the cloud.")
+                }
+        }
+}
